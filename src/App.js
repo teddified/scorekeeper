e27806b6f13761fb404e8356cf2b5eb5208a1de2
teddified <41328906+teddified@users.noreply.github.com>
2018-09-03 12:31:20 +0200

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react'
-import UpdateRate from './UpdateRate'
-import './App.css'
 
-export default class App extends Component {
+export default class extends Component {
   render() {
-    return (
-      <div className="App">
-        <UpdateRate />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
+    return <div>hello world</div>
   }
 }
